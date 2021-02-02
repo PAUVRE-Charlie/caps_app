dde42920d9814a98a735ef47c742fe7eb0f36966
Charlie PAUVREé <chpauvre@gmail.com>
2021-02-03 00:00:37 +0100

    screen rotation blocked

--- lib/main.dart
+++ lib/main.dart
@@ -6,6 +6,7 @@ import 'package:caps_app/services/auth.dart';
 import 'package:caps_app/wrapper.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:provider/provider.dart';
 
 import 'models/capseur.dart';
@@ -20,6 +21,10 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
+    SystemChrome.setPreferredOrientations([
+      DeviceOrientation.portraitUp,
+    ]);
+
     return StreamProvider<BasicUser>.value(
         value: AuthService().user,
         child: MaterialApp(
