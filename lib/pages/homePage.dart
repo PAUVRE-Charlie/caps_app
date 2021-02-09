110ae4e4d5f5b59d725b0484948654435fdeff8b
Charlie PAUVREé <chpauvre@gmail.com>
2021-02-09 18:56:21 +0100

    another try

--- lib/pages/homePage.dart
+++ lib/pages/homePage.dart
@@ -1,22 +1,16 @@
 import 'package:caps_app/components/background.dart';
 import 'package:caps_app/components/loading.dart';
-import 'package:caps_app/components/matchResults.dart';
 import 'package:caps_app/components/validateMatch.dart';
 import 'package:caps_app/models/basicUser.dart';
 import 'package:caps_app/models/capseur.dart';
-import 'package:caps_app/models/matchEnded.dart';
 import 'package:caps_app/models/matchWaitingToBeValidated.dart';
-import 'package:caps_app/pages/lastMatchs.dart';
 import 'package:caps_app/pages/profilePage.dart';
-import 'package:caps_app/pages/randomPickStartPage.dart';
-import 'package:caps_app/pages/rankingPage.dart';
-import 'package:caps_app/pages/rulesPage.dart';
-import 'package:caps_app/pages/tournamentsMenuPage.dart';
 import 'package:caps_app/services/database.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import '../data.dart';
 import '../models/game.dart';
+import 'package:keyboard_visibility/keyboard_visibility.dart';
 
 class HomePage extends StatefulWidget {
   HomePage({Key key}) : super(key: key);
@@ -132,6 +126,30 @@ class MenuOrValidateMatch extends StatefulWidget {
 }
 
 class _MenuOrValidateMatchState extends State<MenuOrValidateMatch> {
+  KeyboardVisibilityNotification _keyboardVisibility =
+      new KeyboardVisibilityNotification();
+  int _keyboardVisibilitySubscriberId;
+  bool _keyboardState;
+
+  @override
+  void initState() {
+    _keyboardState = _keyboardVisibility.isKeyboardVisible;
+    _keyboardVisibilitySubscriberId = _keyboardVisibility.addNewListener(
+      onChange: (bool visible) {
+        setState(() {
+          _keyboardState = visible;
+        });
+      },
+    );
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    _keyboardVisibility.removeListener(_keyboardVisibilitySubscriberId);
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     final matchsWaitingToBeValidated =
@@ -150,64 +168,63 @@ class _MenuOrValidateMatchState extends State<MenuOrValidateMatch> {
       matchWaitingToBeValidated = matchsNotValidatedWhereUserWasOpponent.first;
     }
 
-    return matchsNotValidatedWhereUserWasOpponent.isEmpty
-        ? SingleChildScrollView(
-            child: Center(
-              child: Column(
-                children: <Widget>[
-                  SizedBox(
-                    height: MediaQuery.of(context).size.height / 10,
-                  ),
-                  Text(
-                    "Caps",
-                    style: TextStyle(
-                        fontSize: 100,
-                        fontFamily: 'PirataOne',
-                        color: kSecondaryColor),
-                  ),
-                  SizedBox(
-                    height: MediaQuery.of(context).size.height / 5,
-                  ),
-                  Container(
-                    padding: EdgeInsets.symmetric(horizontal: 50, vertical: 30),
-                    decoration: BoxDecoration(
-                        color: kWhiteHighOpacity,
-                        borderRadius: BorderRadius.circular(20)),
-                    child: Column(
-                      children: [
-                        TextButtonMenu(
-                            text: "Jouer",
-                            onPressed: () {
-                              Game.startMatch(context, "Match", widget.capseur);
-                            }),
-                        // TextButtonMenu(
-                        //     text: "Tournois",
-                        //     onPressed: () {
-                        //       Navigator.push(
-                        //           context,
-                        //           new MaterialPageRoute(
-                        //             builder: (ctxt) => new TournamentsMenuPage(),
-                        //           ));
-                        //     }),
-                        TextButtonMenu(
-                          text: "Classements",
-                          onPressed: () {
-                            Navigator.pushNamed(context, '/rankings');
-                          },
-                        ),
-                        TextButtonMenu(
-                          text: "Derniers matchs",
-                          onPressed: () {
-                            Navigator.pushNamed(context, '/lastmatchs');
-                          },
-                        ),
-                      ],
-                    ),
-                  ),
-                ],
-              ),
+    Widget normalHomePage = Center(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceAround,
+        children: <Widget>[
+          Text(
+            "Caps",
+            style: TextStyle(
+                fontSize: 100, fontFamily: 'PirataOne', color: kSecondaryColor),
+          ),
+          Container(
+            padding: EdgeInsets.symmetric(horizontal: 50, vertical: 30),
+            decoration: BoxDecoration(
+                color: kWhiteHighOpacity,
+                borderRadius: BorderRadius.circular(20)),
+            child: Column(
+              children: [
+                TextButtonMenu(
+                    text: "Jouer",
+                    onPressed: () {
+                      Game.startMatch(context, "Match", widget.capseur);
+                    }),
+                // TextButtonMenu(
+                //     text: "Tournois",
+                //     onPressed: () {
+                //       Navigator.push(
+                //           context,
+                //           new MaterialPageRoute(
+                //             builder: (ctxt) => new TournamentsMenuPage(),
+                //           ));
+                //     }),
+                TextButtonMenu(
+                  text: "Classements",
+                  onPressed: () {
+                    Navigator.pushNamed(context, '/rankings');
+                  },
+                ),
+                TextButtonMenu(
+                  text: "Derniers matchs",
+                  onPressed: () {
+                    Navigator.pushNamed(context, '/lastmatchs');
+                  },
+                ),
+              ],
             ),
-          )
+          ),
+        ],
+      ),
+    );
+
+    return matchsNotValidatedWhereUserWasOpponent.isEmpty
+        ? _keyboardState
+            ? SingleChildScrollView(
+                child: normalHomePage,
+              )
+            : Container(
+                child: normalHomePage,
+              )
         : ValidateMatch(capseurs, matchWaitingToBeValidated);
   }
 }
