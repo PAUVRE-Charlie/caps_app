4aef7ebb0492c1edb05b992e14cef70e0fb58fc8
Charlie PAUVREé <chpauvre@gmail.com>
2021-02-18 18:35:46 +0100

    change disclaimer text

--- lib/components/utils.dart
+++ lib/components/utils.dart
@@ -4,24 +4,28 @@ import 'dart:io';
 Future<void> displayDisclamer(context) {
   return showDialog<void>(
       context: context,
-      builder: (BuildContext context){
+      builder: (BuildContext context) {
         return WillPopScope(
           onWillPop: () {
             return Future.value(false);
           },
           child: AlertDialog(
-            shape:
-            RoundedRectangleBorder(borderRadius: BorderRadius.circular(20.0)),
+            shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(20.0)),
             title: Text("Attention"),
             content: SingleChildScrollView(
-              child: Text("L'abus d'alcool est dangereux pour la santé. En poursuivant vous confirmez être responsable des éventuelles conséquences que pourrait engendrer l'utilisation de Picolo. Sachez que vous n'êtes en aucun cas forcé à boire de l'alcool et nous rappelons que ce jeu peu bienevidemment se jouer sans alcool",
-                style: TextStyle(color: Colors.black, fontSize: 18, fontFamily: "NotoSansJP"),
+              child: Text(
+                "L'abus d'alcool est dangereux pour la santé. En poursuivant vous confirmez être responsable des éventuelles conséquences que pourrait engendrer l'utilisation de Caps Beer Game. Sachez que vous n'êtes en aucun cas forcé à boire de l'alcool et nous rappelons que ce jeu peut bien évidemment se jouer sans alcool",
+                style: TextStyle(
+                    color: Colors.black,
+                    fontSize: 18,
+                    fontFamily: "NotoSansJP"),
               ),
             ),
-            actions: <Widget> [
+            actions: <Widget>[
               TextButton(
                 child: Text("OK"),
-                onPressed: (){
+                onPressed: () {
                   Navigator.pop(context);
                 },
               ),
